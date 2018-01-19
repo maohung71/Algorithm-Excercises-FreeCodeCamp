868f8a488ba557e993f3aa3b96dc9192fdd902b3
mao hung <maohung71@gmail.com>
2018-01-19 12:23:51 -0500

    added reverseString factorial palindrome longestWord

--- main.js
+++ main.js
@@ -40,7 +40,7 @@ console.log(factoraial(5));
 
 
 /*
-2A. Factoralize a number - Using Recursion
+02A. Factoralize a number - Using Recursion
 *** when a function calls itself inside a function.
 */
 
@@ -54,8 +54,85 @@ function factorialRecursion(num){
 
 console.log(factorialRecursion(5));
 
+/*
+03.  Palindrome - Word or sentence that's spelled the same way both forward and backward
+***  using string replace method
+*/
+
+function palindrome(str){
+  //regular expressions for matching everything not a numbers and alphabets
+  var regEx = /[\W_]/g ;
+
+  //replace space, punctuation with empty string
+  var array = str.toLowerCase().replace(regEx, '');
+
+  //reversing the array
+  var strRev = array.split('').reverse().join('');
+
+  return array === strRev;
+
+}
+
+console.log(palindrome("A man, a plan, a canal. Panama"));
+
+/*
+03A. Palindrome - Using Recurion
+***  slice(1, 3) array method selects the elements starting at the given start
+     argument, and ends at, but does not include, the given end argument.
+*/
+
+function palindroneRecursion(str){
+
+  //replace with regular expression
+  str = str.toLowerCase().replace(/[\W_]/g, '');
+
+  if(str.length <= 1){
+    return true;
+  }else if(str[0] === str.slice(-1)){
+    return palindroneRecursion(str.slice(1, -1));
+  }else{
+    return false;
+  }
+}
+
+console.log(palindroneRecursion("radar"));
+
+
+// 04. Find the Longest Word in a String
+
+function findLongestWord(str){
+
+  str = str.split(" ");
+  var longest = 0;
+
+  //console.log(str);
+
+  for(var i = 0; i < str.length; i++){
+    if(str[i].length > longest){
+      longest = str[i].length;
+    }
+  }
+   return longest;
+}
+
+console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
 
 
 
+function findLongest(str){
+
+  str = str.split(" ");
+  var longest = 0;
+
+  //console.log(str);
+  for(var i in str){
+    if(str[i].length > longest){
+      longest = str[i].length;
+    }
+  }
+   return longest;
+}
+
+console.log(findLongest("The quick brown fox jumped over the lazy dog"));
 
 
